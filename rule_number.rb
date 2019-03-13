822bda7a0df4c5cd21274e3c97a5f7e092c398a3
Dan Conrad <dan.m.conrad@gmail.com>
2019-03-13 01:11:11 -0500

    Fixes names

--- rule_number.rb
+++ rule_number.rb
@@ -15,11 +15,7 @@ class RuleNumber
 
   def initialize(value)
     @value = value
-    @parts = value.to_s
-                  .gsub(/[\(\)\. ]+/, ' ')
-                  .gsub(/[ ]+/, ' ')
-                  .split(' ')
-                  .map { |part| classify_part(part) }
+    @parts = to_parts(value)
   end
   
   def to_s
@@ -35,6 +31,14 @@ class RuleNumber
 
   private
 
+  def to_parts(value)
+    value.to_s
+         .gsub(/[\(\)\. ]+/, ' ')
+         .gsub(/[ ]+/, ' ')
+         .split(' ')
+         .map { |part| classify_part(part) }
+  end
+
   def classify_part(part)
     begin
       Integer(part)
@@ -47,62 +51,64 @@ class RuleNumber
     end 
   end
 
-  def compare_parts(first_parts, second_parts)
-    max_parts_length = [first_parts.length, second_parts.length].max
+  def compare_parts(first_parts, other_parts)
+    max_parts_length = [first_parts.length, other_parts.length].max
 
     for idx in 0..max_parts_length
+      # When there are no more parts to compare, 
+      # one set is more specific than the other
       return LESS_THAN if idx == first_parts.length
-      return GREATER_THAN if idx == second_parts.length
+      return GREATER_THAN if idx == other_parts.length
 
       first_part = first_parts[idx]
-      second_part = second_parts[idx]
+      other_part = other_parts[idx]
 
-      if divergent_classes?(first_part, second_part)
+      if divergent_classes?(first_part, other_part)
         # Temporarily Disabled
-        if single_string_and_roman?(first_part, second_part)
-          first_part = first_part.to_s 
-          second_part = second_part.to_s 
-        else
-          return compare_divergent_classes(first_part, second_part)
-        end
+        # if single_string_and_roman?(first_part, other_part)
+        #   first_part = first_part.to_s 
+        #   other_part = other_part.to_s 
+        # else
+        return compare_divergent_classes(first_part, other_part)
+        # end
       end
       
-      value_for_part = value_for(first_part)
-      value_for_other = value_for(second_part)
+      value_for_first = value_for(first_part)
+      value_for_other = value_for(other_part)
 
-      if value_for_part != value_for_other
-        if repeat_strings?(value_for_part, value_for_other)
-          return compare_repeat_strings(value_for_part.to_s, value_for_other.to_s)
+      if value_for_first != value_for_other
+        if repeat_strings?(value_for_first, value_for_other)
+          return compare_repeat_strings(value_for_first, value_for_other)
         end
 
-        return value_for_part <=> value_for_other
+        return value_for_first <=> value_for_other
       end
     end
     
     IDENTICAL
   end
 
-  def repeat_strings?(first, second)
-    return unless first.is_a?(String) && second.is_a?(String) 
-    return if first.length == second.length
-    first.split('').uniq.length == 1 && second.split('').uniq.length == 1
+  def repeat_strings?(first, other)
+    return unless first.is_a?(String) && other.is_a?(String) 
+    return if first.length == other.length
+    first.split('').uniq.length == 1 && other.split('').uniq.length == 1
   end
 
   # Example: bb <=> a
-  def compare_repeat_strings(first, second)
-    return first.length <=> second.length if first[0] == first[0]
-    first[0] <=> second[0]
+  def compare_repeat_strings(first, other)
+    return first.length <=> other.length if first[0] == first[0]
+    first[0] <=> other[0]
   end
 
-  def divergent_classes?(first, second)
-    first.class != second.class
+  def divergent_classes?(first, other)
+    first.class != other.class
   end
 
   # Example: ii <=> 7
-  def compare_divergent_classes(first, second)
+  def compare_divergent_classes(first, other)
     first_priority = CLASS_PRIORITIES.find_index(first.class.name)
-    second_priority = CLASS_PRIORITIES.find_index(second.class.name)
-    first_priority <=> second_priority
+    other_priority = CLASS_PRIORITIES.find_index(other.class.name)
+    first_priority <=> other_priority
   end
 
   # Temporarily Disabled
@@ -110,11 +116,11 @@ class RuleNumber
   #   c could have been interperetted as a RomanNumeral
   #   In the case that both values have a length of 1 and one of those values is 
   #   definitely not a RomanNumeral, we need to treat both as strings
-  def single_string_and_roman?(first, second)
+  def single_string_and_roman?(first, other)
     # return unless first.is_a?(String) || first.is_a?(RomanNumeral)
-    # return unless second.is_a?(String) || second.is_a?(RomanNumeral)
+    # return unless other.is_a?(String) || other.is_a?(RomanNumeral)
     # return unless first.to_s.length == 1
-    # return unless second.to_s.length == 1
+    # return unless other.to_s.length == 1
     # true
     false
   end
